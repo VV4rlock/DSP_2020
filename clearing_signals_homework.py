e175747c22e02046a40b23a0f396dc48ba7d84f0
warlock <Vlad9n>
2020-04-29 21:55:03 +0300

    hw4

---
+++ clearing_signals_homework.py
@@ -0,0 +1,119 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import logging
+from numpy import fft
+from scipy.io import wavfile
+from scipy import signal
+
+SIGNAL1 = r"../dsp_lab/lab_3/0/0.wav"
+SIGNAL2 = r"../dsp_lab/lab_3/1/0.wav"
+
+def show_spec(freqs, fft):
+    plt.figure(figsize=(14, 5))
+    plt.plot(freqs, fft)
+    plt.title('Spectrum')
+    plt.ylabel("Level")
+    plt.xlabel("Frequency")
+    plt.show()
+
+
+def show_filter_spec(b,a, fs):
+    w, h = signal.freqz(b, a, fs=fs)
+    h = np.abs(h)
+    plt.plot(w,h)
+    plt.show()
+
+
+def clear_signal1():
+    sample_rate, x = wavfile.read(SIGNAL1)
+    #pxx, freqs, bins, im = plt.specgram(x, Fs=sample_rate)
+
+    #print(sample_rate, len(x))
+    x_fft = np.abs(fft.rfft(x))
+    freqs = fft.fftfreq(len(x_fft)) * sample_rate / 2
+    i = freqs > 0
+    m = x_fft.max()
+    w1_i = x_fft[i].argmax()
+    w0_i = x_fft[i][:w1_i].argmax()
+    w0,w1 = freqs[i][w0_i],freqs[i][w1_i]
+    print(f"noise freq is {w0} and {w1}")
+    #show_spec(freqs[i], x_fft[i])
+
+    q=200
+    b, a = signal.iirnotch(w0=w0, Q=q, fs=sample_rate)
+
+    #y_1 = signal.lfilter(b, a, x)
+    y_1 = signal.filtfilt(b, a, x)
+
+    b, a = signal.iirnotch(w0=w1, Q=q, fs=sample_rate)
+    y_1 = signal.filtfilt(b, a, y_1)
+
+    plt.figure(figsize=(14, 5))
+    plt.plot(freqs[i], x_fft[i])
+    plt.plot(freqs[i], np.abs(fft.rfft(y_1))[i], alpha=0.5)
+    plt.title('Spectrum')
+    plt.ylabel("Level")
+    plt.xlabel("Frequency")
+    plt.show()
+
+
+    wavfile.write("clear0.wav", sample_rate, y_1)
+
+
+def apply_filter(b, a, x: np.ndarray, window_len=9):
+    print(len(x), len(x) % window_len)
+    pad = window_len - len(x) % window_len
+    x = np.pad(x, (pad, 0), mode='constant')
+    windows = np.split(x, window_len)
+    def filter(i):
+        v = i.var()**0.5
+        # не понятно что даст вычитание(
+        #i = np.where(i < i.mean(), i+v, i-v)
+        return signal.filtfilt(b, a, i)
+    return np.concatenate(list(map(filter, windows)))[pad:]
+
+
+def clear_signal2():
+    sample_rate, x = wavfile.read(SIGNAL2)
+    #pxx, freqs, bins, im = plt.specgram(x, Fs=sample_rate)
+
+    #print(sample_rate, len(x))
+    x_fft = np.abs(fft.rfft(x))
+    freqs = fft.fftfreq(len(x_fft)) * sample_rate / 2
+    i = freqs > 0
+
+    VOICE_HIGH_FREQ = 4000
+    VOICE_LOW_FREQ = 110
+    q=5
+    b,a = signal.butter(q, Wn=(VOICE_LOW_FREQ, VOICE_HIGH_FREQ), btype='bandpass', fs=sample_rate)
+
+    show_filter_spec(b, a, sample_rate)
+    #y_1 = signal.lfilter(b, a, x)
+    y_1 = apply_filter(b, a, x)
+    print(len(y_1), len(x))
+
+    '''
+    window_len = 551
+    window = signal.gaussian(window_len, 3)
+    window = window / window.sum()
+    print(len(y_1), y_1.shape)
+    y_1 = cv2.filter2D(x, -1, window).reshape(-1)
+    print(len(y_1), y_1.shape)
+    '''
+    plt.figure(figsize=(14, 5))
+    plt.plot(freqs[i], x_fft[i])
+    plt.plot(freqs[i], np.abs(fft.rfft(y_1))[i], alpha=0.5)
+    plt.title('Spectrum')
+    plt.ylabel("Level")
+    plt.xlabel("Frequency")
+    plt.show()
+
+
+    wavfile.write("clear1.wav", sample_rate, y_1)
+
+
+if __name__=="__main__":
+    clear_signal1()
+    clear_signal2()
--- modulation_homework.py
+++ modulation_homework.py
@@ -4,7 +4,7 @@ from scipy.io import wavfile
 from functools import reduce
 from scipy.signal import butter, lfilter
 
-MSG = b"THIS_messageIsNotSecret"
+MSG = b"FINAL"
 
 freq_array = {"C": 261.63, "D": 293.66, "E": 329.63, "F": 349.23, "G": 392.00, "A": 440.00, "B": 493.88, "_": 0}
 
@@ -17,7 +17,7 @@ mod_sequence = np.array(list(map(int, reduce(lambda a, x: a+list(bin(x)[2:].zfil
 
 
 SAMPLERATE = 16000
-CHR_PER_SEC = 10
+CHR_PER_SEC = 4
 AMPL = np.iinfo(np.int16).max
 WIDTH = SAMPLERATE//CHR_PER_SEC
 
@@ -41,34 +41,23 @@ def get_fsk_signal(mod_sequence, mod_1_freq=128, tick_freq=3000, mod_0_freq=None
     return AMPL // 2 * (np.sin(mod_frequencies * 2.0 * np.pi * t) + np.sin(tick_signal * 2.0 * np.pi * t))
 
 
-
-def butter_bandpass(lowcut, highcut, fs, order=5):
-    nyq = 0.5 * fs
-    low = lowcut / nyq
-    high = highcut / nyq
-    print(low, high)
-    b, a = butter(order, (low, high), btype='bandpass', output='ba')
-    return b, a
-
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=7):
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-    y = lfilter(b, a, data)
-    return y
-
 def get_message_from_record(filename):
     fs, signal = wavfile.read(filename)
     print(fs)
     #signal = butter_bandpass_filter(signal, 1000, 4000, fs)
     pxx, freqs, bins, im = plt.specgram(signal, Fs=fs)
     plt.show()
-    T = 2000
+    T = 15000
+    print(freqs)
     high = pxx[np.abs(freqs - high_freq).argmin()]
-
+    plt.plot(high)
+    plt.show()
     high = np.where(high < T, 0, 1)
 
     tick = pxx[np.abs(freqs - tick_freq).argmin()]
     tick = np.where(tick < T, 0, 1)
     plt.plot(high)
+
     plt.plot(tick)
     plt.show()
 
@@ -82,8 +71,11 @@ def get_message_from_record(filename):
         word.append(chr(int(''.join(map(str, res[i: i + 8])), 2)))
     return ''.join(word)
 
-
+GENERATE = False
 if __name__=="__main__":
-    fsk_signal = get_fsk_signal(mod_sequence, mod_1_freq=high_freq, tick_freq=tick_freq)
-    wavfile.write("fsk_signal.wav", SAMPLERATE, fsk_signal)
-    print(f"result: {get_message_from_record('My recording 2.wav')}")
+    if GENERATE:
+        fsk_signal = get_fsk_signal(mod_sequence, mod_1_freq=high_freq, tick_freq=tick_freq)
+        pxx, freqs, bins, im = plt.specgram(fsk_signal, Fs=16000)
+        plt.show()
+        wavfile.write("fsk_signal.wav", SAMPLERATE, fsk_signal)
+    print(f"result: {get_message_from_record('fsk_signal_message.wav')}")
